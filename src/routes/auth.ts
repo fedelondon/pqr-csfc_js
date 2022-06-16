9c26d30dcd73a820faf854d19bd8e2454f360327
fedelondon <felova21@gmail.com>
2022-06-16 09:18:07 -0500

    Require Response in expres routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,8 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 
 const router: Router = Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req: Request, res: Response) => {
   res.json({ message: "Hello World" });
 });
 
